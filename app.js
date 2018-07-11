8a085f7a5c6120e683f289ec4a100f7255d80863
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-07-11 17:09:27 +0200

    basic project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const chalk = require('chalk')
+
+const ejs=require('ejs')
+const app = new express()
+
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.set('views','./src/views')
+app.set('view engine','ejs')
+
+app.get('/', (req, res) => {
+  res.render('index',{title:'MR:Error',frinds:['Ali','Abas','Issac','Ahmad']});
+});
+
+app.listen(5000, () => {
+  console.log(`listening on port ${chalk.blue('5000')}`);
+});
